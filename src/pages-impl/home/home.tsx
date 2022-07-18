5b3f15b772de98c77723a4cde457e39daf06c4dd
Nadine <nadlgit@users.noreply.github.com>
2022-07-17 22:47:43 -0400

    modified project structure

--- src/pages-impl/home/home.tsx
+++ src/pages-impl/home/home.tsx
@@ -1,13 +1,3 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-
-export const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>My Pizza</title>
-        <meta name="description" content="Simulation d'un site de commande de pizza" />
-      </Head>
-    </>
-  );
+export const Home = () => {
+  return <div></div>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
 import { Home } from 'pages-impl/home';
-export default Home;
+
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>My Pizza</title>
+        <meta name="description" content="Simulation d'un site de commande de pizza" />
+      </Head>
+      <Home />
+    </>
+  );
+};
+
+export default HomePage;
