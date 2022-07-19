7c921bbaac1b8fbf7e2e096441818c1ef0eae757
Nadine <nadlgit@users.noreply.github.com>
2022-07-19 19:11:13 -0400

    added ingredients data

---
+++ src/data/bases/index.ts
@@ -0,0 +1,4 @@
+import tomato from './tomato.json';
+import cream from './cream.json';
+
+export const bases = [tomato, cream];
---
+++ src/data/ingredients/index.ts
@@ -0,0 +1,27 @@
+import ham from './ham.json';
+import cheese from './cheese.json';
+import chorizo from './chorizo.json';
+import mushrooms from './mushrooms.json';
+import olive from './olive.json';
+import onions from './onions.json';
+import chicken from './chicken.json';
+import lardoons from './lardoons.json';
+import merguez from './merguez.json';
+import egg from './egg.json';
+import salmon from './salmon.json';
+import queenconch from './queen-conch.json';
+
+export const ingredients = [
+  ham,
+  cheese,
+  chorizo,
+  mushrooms,
+  olive,
+  onions,
+  chicken,
+  lardoons,
+  merguez,
+  egg,
+  salmon,
+  queenconch,
+];
