0a402752bab4ec3d1a22582985642eb87e5e092e
Nadine <nadlgit@users.noreply.github.com>
2022-07-20 00:01:00 -0400

    replace <div> by <span> in Image component

--- src/shared/components/ui/image/image.tsx
+++ src/shared/components/ui/image/image.tsx
@@ -5,8 +5,8 @@ type ImageProps = Pick<NextImageProps, 'src' | 'alt' | 'className'>;
 
 export const Image = ({ src, alt, className }: ImageProps) => {
   return (
-    <div className={`${styles.image} ${className}`}>
+    <span className={`${styles.imgwrapper} ${className}`}>
       <NextImage src={src} alt={alt} layout="fill" />
-    </div>
+    </span>
   );
 };
