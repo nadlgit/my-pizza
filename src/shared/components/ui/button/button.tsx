58a47711cfbc6a93d6a47ca561ecec3d92cc3465
Nadine <nadlgit@users.noreply.github.com>
2022-08-09 16:47:47 -0400

    fixed Button default type

--- src/shared/components/ui/button/button.tsx
+++ src/shared/components/ui/button/button.tsx
@@ -4,9 +4,13 @@ import type { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
-export const Button = ({ className, children, ...otherprops }: ButtonProps) => {
+export const Button = ({ className, children, type, ...otherprops }: ButtonProps) => {
   return (
-    <button className={`${styles.button}  ${className ?? ''}`} {...otherprops}>
+    <button
+      type={type ?? 'button'}
+      className={`${styles.button}  ${className ?? ''}`}
+      {...otherprops}
+    >
       {children}
     </button>
   );
