071979e5e84d989f5107ddd5aeec0fdbe10651b8
Nadine <nadlgit@users.noreply.github.com>
2022-08-12 22:45:31 -0400

    test: updateOrderAmount function

---
+++ src/data/order/helpers.test.ts
@@ -0,0 +1,77 @@
+import { updateOrderAmount } from './helpers';
+import { DELIVERY_EXTRA_CHARGE } from './constants';
+
+import type { order, ingredient } from 'data/model';
+
+describe('updateOrderAmount', () => {
+  const fakePizzaBase: ingredient = {
+    id: 'base-id',
+    title: 'Base',
+    price: 8,
+    imgUrl: 'pizza-base.png',
+    previewUrl: 'preview-pizza-base.png',
+  };
+
+  const fakePizzaIngredients: ingredient[] = [
+    {
+      id: 'ingr-id1',
+      title: 'Ingredient1',
+      price: 0.75,
+      imgUrl: 'pizza-ingredient1.png',
+      previewUrl: 'preview-pizza-ingredient1.png',
+    },
+    {
+      id: 'ingr-id2',
+      title: 'Ingredient2',
+      price: 1.1,
+      imgUrl: 'pizza-ingredient2.png',
+      previewUrl: 'preview-pizza-ingredient2.png',
+    },
+  ];
+
+  it('should return new object with calculated amount', () => {
+    const value: order = {
+      id: 1234,
+      base: { ...fakePizzaBase },
+      ingredients: [...fakePizzaIngredients],
+      amount: 0,
+      amountExtraPart: 0,
+      deliveryMode: 'pick-up',
+    };
+    const expected: order = {
+      ...value,
+      amount:
+        fakePizzaBase.price + fakePizzaIngredients.reduce((price, item) => price + item.price, 0),
+      amountExtraPart: 0,
+    };
+    expect(value).not.toEqual(expected);
+
+    const res = updateOrderAmount(value);
+    expect(res).not.toBe(value);
+    expect(res).toEqual(expected);
+  });
+
+  it('should apply extra charge for delivery', () => {
+    const value: order = {
+      id: 98,
+      base: { ...fakePizzaBase },
+      ingredients: [...fakePizzaIngredients],
+      amount: 0,
+      amountExtraPart: 0,
+      deliveryMode: 'delivery',
+    };
+    const expected = {
+      ...value,
+      amount:
+        fakePizzaBase.price +
+        fakePizzaIngredients.reduce((price, item) => price + item.price, 0) +
+        DELIVERY_EXTRA_CHARGE,
+      amountExtraPart: DELIVERY_EXTRA_CHARGE,
+    };
+    expect(value).not.toEqual(expected);
+
+    const res = updateOrderAmount(value);
+    expect(res).not.toBe(value);
+    expect(res).toEqual(expected);
+  });
+});
