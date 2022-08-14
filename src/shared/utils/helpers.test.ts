3d7a7aac823050ff6c20583c051778b47c7c3ab8
Nadine <nadlgit@users.noreply.github.com>
2022-08-13 21:10:29 -0400

    updated test name

--- src/shared/utils/helpers.test.ts
+++ src/shared/utils/helpers.test.ts
@@ -6,7 +6,7 @@ describe('formatPrice', () => {
     [0.7, '0,70€'],
     [19.386, '19,39€'],
     [5.412, '5,41€'],
-  ])('should format %f', (value, expected) => {
+  ])('should format %f as %s', (value, expected) => {
     expect(formatPrice(value)).toBe(expected);
   });
 });
