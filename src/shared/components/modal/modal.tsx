2c8de9c6260fd78c88d61b8136d684546536771b
Nadine <nadlgit@users.noreply.github.com>
2022-08-15 20:49:34 -0400

    added focus trap to Modal component

--- src/shared/components/modal/modal.tsx
+++ src/shared/components/modal/modal.tsx
@@ -1,19 +1,38 @@
 import styles from './Modal.module.css';
 import { createPortal } from 'react-dom';
 import { useMounted } from 'shared/utils/use-mounted';
-import { ReactNode } from 'react';
+import FocusTrap from 'focus-trap-react';
+import { useEffect, useState } from 'react';
+
+import type { ReactNode } from 'react';
 
 type ModalProps = { isOpen: boolean; close: () => void; children: ReactNode };
 
 export const Modal = ({ isOpen, close, children }: ModalProps) => {
   const isMounted = useMounted();
 
+  const [trapActive, setTrapActive] = useState(false);
+  useEffect(() => {
+    setTrapActive(isMounted && isOpen);
+  }, [isMounted, isOpen]);
+
   return isMounted && isOpen
     ? createPortal(
         <div className={styles.backdrop}>
-          <div className={styles.content} role="dialog">
-            {children}
-          </div>
+          <FocusTrap
+            active={trapActive}
+            focusTrapOptions={{
+              onDeactivate: () => {
+                close();
+              },
+              escapeDeactivates: true,
+              clickOutsideDeactivates: true,
+            }}
+          >
+            <div className={styles.content} role="dialog">
+              {children}
+            </div>
+          </FocusTrap>
         </div>,
         document.getElementById('modal') as Element
       )
