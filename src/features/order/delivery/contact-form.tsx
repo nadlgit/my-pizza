85790a805e7c5e631e0396d19446e6e39df77d96
Nadine <nadlgit@users.noreply.github.com>
2022-08-15 21:31:00 -0400

    continued ContactForm component

--- src/features/order/delivery/contact-form.tsx
+++ src/features/order/delivery/contact-form.tsx
@@ -1,4 +1,6 @@
 import styles from './contact-form.module.css';
+import { Button } from 'shared/components/ui/button';
+import { useState } from 'react';
 
 import type { order } from 'data/model';
 
@@ -11,21 +13,32 @@ type ContactFormProps = {
 };
 
 export const ContactForm = ({ contact, onChange, onClose }: ContactFormProps) => {
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
   const handleSubmit = () => {
     // onChange({});
     onClose();
   };
+
   return (
-    <form>
-      <p>TEST</p>
+    <form onSubmit={handleSubmit} className={styles.container}>
       <p>{contact?.name}</p>
       <p>{contact?.address?.line1}</p>
       <p>{contact?.address?.line2}</p>
       <p>{contact?.address?.city}</p>
       <p>{contact?.phoneNumber}</p>
-      <button type="button" onClick={() => onClose()}>
-        Fermer
-      </button>
+      <div className={styles.btns}>
+        <Button className={styles.cancelbtn} onClick={() => onClose()}>
+          Annuler
+        </Button>
+        <Button
+          className={submitDisabled ? styles.disabledbtn : styles.submitbtn}
+          type="submit"
+          disabled={submitDisabled}
+        >
+          Valider
+        </Button>
+      </div>
     </form>
   );
 };
