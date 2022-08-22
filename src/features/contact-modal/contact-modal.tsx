b8f772404e35005f9c9450db05a5a095086716cd
Nadine <nadlgit@users.noreply.github.com>
2022-08-21 21:07:38 -0400

    switched to react-hook-form library for ContactModal component

--- src/features/contact-modal/contact-modal.tsx
+++ src/features/contact-modal/contact-modal.tsx
@@ -1,9 +1,11 @@
 import styles from './contact-modal.module.css';
 import { Modal } from 'shared/components/modal';
 import { Button } from 'shared/components/ui/button';
+import { useForm } from 'react-hook-form';
 
 import type { Order } from 'data/model';
 import type { FormEventHandler } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
 
 type Contact = Order['contact'];
 
@@ -14,91 +16,127 @@ type ContactModalProps = {
   onClose: () => void;
 };
 
-function formInputToString(data: FormDataEntryValue | null) {
-  if (typeof data !== 'string' || data.trim() === '') {
-    return undefined;
-  }
-  return data.trim();
-}
+type FormValues = {
+  name: string;
+  addrLine1: string;
+  addrLine2: string;
+  city: string;
+  phone: string;
+};
 
 export const ContactModal = ({ isOpen, contact, onChange, onClose }: ContactModalProps) => {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+    defaultValues: {
+      name: contact?.name ?? '',
+      addrLine1: contact?.address?.line1 ?? '',
+      addrLine2: contact?.address?.line2 ?? '',
+      city: contact?.address?.city ?? '',
+      phone: contact?.phoneNumber ?? '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    onChange({
+      name: data.name,
+      address: {
+        line1: data.addrLine1,
+        line2: data.addrLine2 === '' ? undefined : data.addrLine2,
+        city: data.city,
+      },
+      phoneNumber: data.phone,
+    });
+    onClose();
+  };
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     e.stopPropagation();
-
-    const data = new FormData(e.target as HTMLFormElement);
-    onChange({
-      name: formInputToString(data.get('name')),
-      address: {
-        line1: formInputToString(data.get('addrLine1')),
-        line2: formInputToString(data.get('addrLine2')),
-        city: formInputToString(data.get('city')),
-      },
-      phoneNumber: formInputToString(data.get('phone')),
-    } as Contact);
-
-    onClose();
+    handleSubmit(onSubmit)(e);
   };
 
   return (
     <Modal isOpen={isOpen} close={onClose}>
-      <form onSubmit={handleSubmit} className={styles.container}>
+      <form onSubmit={handleFormSubmit} className={styles.container}>
         <h2> Vos coordonnées</h2>
 
         <label htmlFor="name">Nom</label>
         <input
           id="name"
-          name="name"
-          defaultValue={contact?.name}
           type="text"
-          pattern="[^\s].*"
-          required
+          {...register('name', {
+            setValueAs: (txt) => txt.trim(),
+            required: 'Vous devez indiquer votre nom',
+            pattern: {
+              value: /^[a-zA-Z]/,
+              message: 'Votre nom doit commencer par une lettre',
+            },
+          })}
         />
+        <span>{errors?.name?.message}</span>
 
         <label htmlFor="addrLine1">Adresse</label>
         <input
           id="addrLine1"
-          name="addrLine1"
-          defaultValue={contact?.address?.line1}
           type="text"
-          pattern="[^\s].*"
-          required
+          {...register('addrLine1', {
+            setValueAs: (txt) => txt.trim(),
+            required: 'Vous devez indiquer votre adresse',
+            pattern: {
+              value: /^[a-zA-Z0-9]/,
+              message: 'Votre adresse doit commencer par une lettre ou un chiffre',
+            },
+          })}
         />
+        <span>{errors?.addrLine1?.message}</span>
 
         <label htmlFor="addrLine2">Complément d&apos;adresse (optionnel)</label>
         <input
           id="addrLine2"
-          name="addrLine2"
-          defaultValue={contact?.address?.line2}
           type="text"
-          pattern="[^\s].*"
+          {...register('addrLine2', { setValueAs: (txt) => txt.trim() })}
         />
 
         <label htmlFor="city">Ville</label>
         <input
           id="city"
-          name="city"
-          defaultValue={contact?.address?.city}
           type="text"
-          pattern="[^\s].*"
-          required
+          {...register('city', {
+            setValueAs: (txt) => txt.trim(),
+            required: 'Vous devez indiquer votre ville',
+            pattern: {
+              value: /^[a-zA-Z]/,
+              message: 'Votre ville doit commencer par une lettre',
+            },
+          })}
         />
+        <span>{errors?.city?.message}</span>
 
         <label htmlFor="phone">Numéro de téléphone</label>
         <input
           id="phone"
-          name="phone"
-          defaultValue={contact?.phoneNumber}
           type="text"
-          pattern="[^\s].*"
-          required
+          {...register('phone', {
+            setValueAs: (txt) => txt.trim(),
+            required: 'Vous devez indiquer votre numéro de téléphone',
+            pattern: {
+              value: /^[-+()\.\d\s]+$/,
+              message:
+                'Votre numéro de téléphone ne doit comporter que des chiffres ou les caractères "+ - ( ) ."',
+            },
+          })}
         />
+        <span>{errors?.phone?.message}</span>
 
         <div className={styles.btns}>
           <Button color="red" onClick={() => onClose()}>
             Annuler
           </Button>
-          <Button color="green" type="submit">
+          <Button color="green" type="submit" disabled={!isValid}>
             Valider
           </Button>
         </div>
